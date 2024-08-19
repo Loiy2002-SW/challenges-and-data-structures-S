bf1311238429b89c46efce2865a847e6319f7cfb
loiy <lloiy9310@gmail.com>
2024-08-19 15:44:46 +0300

    Add the classes

---
+++ challenges-and-data-structures/DataStructures/Trees/TreeImplementationS/SecondMaxValue/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace SecondMaxValue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree Btree = new BinaryTree();
+            Btree.Root = new Node(10);
+            Btree.Root.Left = new Node(5);
+            Btree.Root.Right = new Node(20);
+            Btree.Root.Left.Left = new Node(3);
+            Btree.Root.Left.Right = new Node(7);
+            Btree.Root.Right.Left = new Node(15);
+            Btree.Root.Right.Right = new Node(25);
+
+            try
+            {
+                int secondMax = Btree.FindSecondMax();
+                Console.WriteLine($"Second Maximum Value: {secondMax}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
