93210c2fc2e21e2f15751a1c43f3b02fc388067c
loiy <lloiy9310@gmail.com>
2024-08-21 15:19:03 +0300

    Add BinaryTree class with the sumation method

--- challenges-and-data-structures/DataStructures/Trees/TreeImplementationS/LeafSum/BinaryTree.cs
+++ challenges-and-data-structures/DataStructures/Trees/TreeImplementationS/LeafSum/BinaryTree.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LeafSum
+﻿namespace LeafSum
 {
-    internal class BinaryTree
+    public class BinaryTree
     {
+        public Node Root { get; set; }
+
+        public int SumOfLeafNodes()
+        {
+            return SumOfLeafNodes(Root);
+        }
+
+        private int SumOfLeafNodes(Node node)
+        {
+            if (node == null)
+                return 0;
+
+            // If node is a leaf
+            if (node.Left == null && node.Right == null)
+                return node.Value;
+
+            // Recursively sum leaf nodes from left and right subtrees
+            return SumOfLeafNodes(node.Left) + SumOfLeafNodes(node.Right);
+        }
     }
 }
