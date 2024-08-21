4509c774f93a246291d3d4ecf3fd5a2611007a67
loiy <lloiy9310@gmail.com>
2024-08-21 15:20:10 +0300

    Update the main method to use SumOfLeafNodes method

--- challenges-and-data-structures/DataStructures/Trees/TreeImplementationS/LeafSum/Program.cs
+++ challenges-and-data-structures/DataStructures/Trees/TreeImplementationS/LeafSum/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BinaryTree Btree = new BinaryTree();
+            Btree.Root = new Node(9);
+            Btree.Root.Left = new Node(8);
+            Btree.Root.Right = new Node(12);
+            Btree.Root.Left.Left = new Node(3);
+            Btree.Root.Left.Right = new Node(7);
+            Btree.Root.Right.Left = new Node(17);
+            Btree.Root.Right.Right = new Node(23);
+            Btree.Root.Left.Left.Right = new Node(4);
+
+            int leafSum = Btree.SumOfLeafNodes(); // Output: 51
+            Console.WriteLine($"The sum of all leaf nodes is: {leafSum}");
         }
     }
 }
