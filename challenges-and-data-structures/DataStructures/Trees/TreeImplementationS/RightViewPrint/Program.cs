1a01ae4f458e4d0ee821eb0e6657a88b2b82da64
loiy <lloiy9310@gmail.com>
2024-09-28 14:14:33 +0300

    Implement the Node, BinaryTree

---
+++ challenges-and-data-structures/DataStructures/Trees/TreeImplementationS/RightViewPrint/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace RightViewPrint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree Btree = new BinaryTree();
+            Btree.Root = new Node(2);
+            Btree.Root.Left = new Node(3);
+            Btree.Root.Right = new Node(5);
+            Btree.Root.Left.Left = new Node(4);
+            Btree.Root.Right.Right = new Node(6);
+            Btree.Root.Left.Left.Right = new Node(7);
+
+            Btree.PrintRightView();  // Expected output: 2 5 6 7
+        }
+    }
+}
