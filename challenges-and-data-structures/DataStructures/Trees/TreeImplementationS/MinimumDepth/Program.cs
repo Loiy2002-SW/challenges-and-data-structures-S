bcf1289c4a7902256d2c5d0b1625b7ba872244f4
loiy <lloiy9310@gmail.com>
2024-10-08 17:08:22 +0300

    Update the main method

--- challenges-and-data-structures/DataStructures/Trees/TreeImplementationS/MinimumDepth/Program.cs
+++ challenges-and-data-structures/DataStructures/Trees/TreeImplementationS/MinimumDepth/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BinaryTree Btree = new BinaryTree();
+            Btree.Root = new Node(1);
+            Btree.Root.Left = new Node(2);
+            Btree.Root.Right = new Node(3);
+            Btree.Root.Left.Left = new Node(4);
+            Btree.Root.Left.Right = new Node(5);
+
+            Console.WriteLine(Btree.FindMinimumDepth()); // Output: 2
+
+            Btree.Root.Left.Right = null;
+            Btree.Root.Right.Right = new Node(6);
+
+            Console.WriteLine(Btree.FindMinimumDepth()); // Output: 3
         }
     }
 }
